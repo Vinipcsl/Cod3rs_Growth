59389fec390c439b853f0a099a2f2ea547e4d659
Vinipcsl <viniciuspcsl@gmail.com>
2023-04-14 10:12:28 -0300

    Atualizando a modelagem de dados

--- Cod3rs_Growth/Cod3r’s_Growth/Celular.cs
+++ Cod3rs_Growth/Cod3r’s_Growth/Celular.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -9,64 +10,18 @@ namespace Modelo_de_Dados
 {
     public class Celular
     {
-        private string Marca;
-        private string Modelo;
-        private string Cor;
-        private string AnoFabriacao;
-        private int Memoria, Id;
+        public int Id { get; set; }
+        public string Marca { get; set; }
 
-        public Celular(string marca, string modelo, string cor, string anoFabriacao, int memoria, int id)
-        {
-            Marca = marca;
-            Modelo = modelo;
-            Cor = cor;
-            AnoFabriacao = anoFabriacao;
-            Memoria = memoria;
-            Id = id;
-        }
+        public string Modelo { get; set; }
 
+        public string Cor { get; set; }
 
-        public string marca
-        {
-            get
-            { return marca; }
-            set
-            { marca = value; }
-        }
-        public string modelo
-        {
-            get
-            { return modelo; }
-            set
-            { modelo = value; }
-        }
-        public string cor
-        {
-            get
-            { return cor; }
-            set
-            { cor = value; }
-        }
-        public string anoFabriacao
-        {
-            get { return cor; }
-            set { cor = value; }
-        }
-        public int memoria
-        {
-            get { return memoria; }
-            set { memoria = value; }
-        }
-        public int id
-        {
-            get { return id; }
-            set { id = value; }
-        }
+        public DateTime AnoFabricacao { get; set; }
 
+        public int Memoria { get; set; }
 
-
-
-
+        
 
 
     }
