06f978085b22b56321c5e2bbf971f2273cde5f54
Vinipcsl <viniciuspcsl@gmail.com>
2023-05-19 13:37:49 -0300

    Na parte das querys de adicionar funcionando, atualizar com problema pois atualiza tudo, deletar ta funcionando mas o datagrid não atualiza na mesma velocidade.

--- Cod3rs_Growth/Cod3rs_Growth/Cod3r’s_Growth/RepositorioBanco/RepositorioDoBanco.cs
+++ Cod3rs_Growth/Cod3rs_Growth/Cod3r’s_Growth/RepositorioBanco/RepositorioDoBanco.cs
@@ -3,7 +3,6 @@ using Modelo_de_Dados;
 using System.ComponentModel;
 using System.Configuration;
 using Microsoft.Data.SqlClient;
-using System.Data.Common;
 namespace Cod3r_s_Growth.RepositorioBanco
 {
     internal class RepositorioDoBanco : IRepositorio
@@ -14,11 +13,14 @@ namespace Cod3r_s_Growth.RepositorioBanco
 
         public void Adicionar(Celular novoCelular)
         {
-            string sql = "INSERT INTO Celulares(Marca, Modelo, Cor, Memoria, AnoFabricado" +
-                "Values" + "@Marca, @Modelo,@Cor,@Memoria,@AnoFabricado";
+            string sql = "INSERT INTO Celulares (Marca,Modelo,Cor,Memoria,AnoFabricado)" +
+                "Values" +
+                "(@Marca,@Modelo,@Cor,@Memoria,@AnoFabricado)";
+
             using (SqlConnection connection = new SqlConnection(CadastroCelular))
             {
                 connection.Open();
+
                 using (SqlCommand command = new(sql, connection))
                 {
                     command.Parameters.AddWithValue("@Marca", novoCelular.Marca);
@@ -26,24 +28,32 @@ namespace Cod3r_s_Growth.RepositorioBanco
                     command.Parameters.AddWithValue("@Cor", novoCelular.Cor);
                     command.Parameters.AddWithValue("@Memoria", novoCelular.Memoria);
                     command.Parameters.AddWithValue("@AnoFabricado", novoCelular.AnoFabricacao);
-
                     command.ExecuteNonQuery();
-
                 }
+             
             }
-          
         }
 
         public void Atualizar(int id, Celular novoCelular)
         {
-            string sql = "INSERT INTO Celulares(Marca, Modelo, Cor, Memoria, AnoFabricado" +
-               "Values" + "@Marca, @Modelo,@Cor,@Memoria,@AnoFabricado";
+            string sql = "Update Celulares (Marca,Modelo,Cor,Memoria,AnoFabricado)" +
+                "Values" +
+                "(@Marca,@Modelo,@Cor,@Memoria,@AnoFabricado)";
+
             using (SqlConnection connection = new SqlConnection(CadastroCelular))
             {
                 connection.Open();
                 SqlCommand command = new(sql, connection);
-                command.ExecuteNonQuery();
-            }
+                {
+                    command.Parameters.AddWithValue("@Marca", novoCelular.Marca);
+                    command.Parameters.AddWithValue("@Modelo", novoCelular.Modelo);
+                    command.Parameters.AddWithValue("@Cor", novoCelular.Cor);
+                    command.Parameters.AddWithValue("@Memoria", novoCelular.Memoria);
+                    command.Parameters.AddWithValue("@AnoFabricado", novoCelular.AnoFabricacao);
+                    command.ExecuteNonQuery();
+                }
+            }   
+
         }
 
         public void Deletar(int id)
@@ -59,12 +69,64 @@ namespace Cod3r_s_Growth.RepositorioBanco
 
         public Celular? ObterPorId(int id)
         {
-            throw new Exception();
+            Celular celular = new();
+
+            string sql = $"Select * from Celulares where id{id}";
+
+            using (SqlConnection connection = new SqlConnection(CadastroCelular))
+            {
+
+                connection.Open();
+                SqlCommand command = new(sql, connection);
+                SqlDataReader read = command.ExecuteReader();
+                _listaCelulares.Clear();
+
+                while (read.Read())
+                {
+                    Celular celularId = new()
+                    {
+                        Id = (int)read.GetInt64(0),
+                        Marca = (string)read.GetString(1),
+                        Modelo = (string)read.GetString(2),
+                        Cor = (string)read.GetString(3),
+                        Memoria = (int)read.GetInt32(4),
+                        AnoFabricacao = (DateTime)read.GetDateTime(5),
+
+                    };
+                    celular = celularId;
+                }
+            }
+            return celular;
         }
 
         public BindingList<Celular> ObterTodos()
         {
-            throw new Exception();
+            Celular celular = new();
+
+            string sql = "Select * from Celulares";
+
+            using (SqlConnection connection = new SqlConnection(CadastroCelular))
+            {
+                connection.Open();
+                SqlCommand command = new(sql, connection);
+                SqlDataReader read = command.ExecuteReader();
+                _listaCelulares.Clear();
+                while (read.Read())
+                {
+                    celular = new()
+                    {
+                        Id = (int)read.GetInt64(0),
+                        Marca = (string)read.GetString(1),
+                        Modelo = (string)read.GetString(2),
+                        Cor = (string)read.GetString(3),
+                        Memoria = (int)read.GetInt32(4),
+                        AnoFabricacao = (DateTime)read.GetDateTime(5),
+
+                    };
+                    _listaCelulares.Add(celular);
+                }
+            }
+            return _listaCelulares;
         }
     }
 }
