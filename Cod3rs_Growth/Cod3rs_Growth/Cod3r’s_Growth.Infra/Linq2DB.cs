66f09b6e5ec2c7cd921e688a3a456d3bbc02aa6c
Vinipcsl <viniciuspcsl@gmail.com>
2023-05-29 11:29:42 -0300

    Iniciando o Linq2DB

---
+++ Cod3rs_Growth/Cod3rs_Growth/Cod3r’s_Growth.Infra/Linq2DB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3r_s_Growth.Infra
+{
+    internal class Linq2DB
+    {
+    }
+}
